97e5176ef2411c7fb581085d6d74991f95f7d9a2
Edward Swernofsky <edabsw@gmail.com>
2015-09-04 16:21:54 -0700

    145 now 18 seconds in C but looking a little stale

--- 145.c
+++ 145.c
@@ -15,27 +15,25 @@ bool odd_digits(int n) {
   }
 }
 
-int magnitude(int n) {
-  if (n < 10) {
-    return 1;
-  } else {
-    return 10 * magnitude(n / 10);
-  }
-}
-
 int main(void) {
   int MAX = 1e9;
   int count = 0;
   int* reverse = malloc(MAX * sizeof(int));
+  int* magnitude = malloc(MAX / 10 * sizeof(int));
 
   for (int n = 0; n < 10; n++) {
     reverse[n] = n;
+    magnitude[n] = 1;
+  }
+
+  for (int n = 10; n < MAX / 10; n++) {
+    magnitude[n] = 10 * magnitude[n / 10];
   }
 
   for (int n = 10; n < MAX; n++) {
     int digit = n % 10;
 
-    int reverse_int = digit * magnitude(n) + reverse[n / 10];
+    int reverse_int = digit * 10 * magnitude[n / 10] + reverse[n / 10];
     reverse[n] = reverse_int;
     
     if (n % 10 != 0 && odd_digits(n + reverse_int)) {
